adcad010985b85388ba8f5d82c48a0096266057f
戴均民 <taichunmin@gmail.com>
2021-10-11 04:08:15 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-require('dotenv').config() // 將 dotenv 讀取到環境變數中
+require("dotenv").config(); // 將 dotenv 讀取到環境變數中
 
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const liffRouter = require("./routes/liff");
 
-const app = express()
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'))
-app.use('/webhook', require('./line')) // 需要在 express.json() 之前
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
+app.use("/webhook", require("./line")); // 需要在 express.json() 之前
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/liff", liffRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-module.exports = app
+module.exports = app;
