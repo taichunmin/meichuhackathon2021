16e46a68cd6e2dc5954c7210641abc132379de9e
taichunmin <taichunmin@gmail.com>
2021-10-12 15:24:49 +0800

    20211012

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 require('dotenv').config() // 將 dotenv 讀取到環境變數中
 
+const { getenv } = require('./libs/helper')
+const cookieParser = require('cookie-parser')
 const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const path = require('path')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const liffRouter = require('./routes/liff')
 
 const app = express()
+app.locals.NODE_ENV = getenv('NODE_ENV', 'development')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
